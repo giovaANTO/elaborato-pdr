0bff345a3cec27c17dee0bfe40a6e736f1982ed6
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2021-06-21 01:03:12 +0200

    Add role class

---
+++ src/roles.py
@@ -0,0 +1,20 @@
+"""
+Utility used for selecting a role in the game.
+"""
+
+import random
+
+
+def random_role():
+    """
+    Select a random role from the list
+    :return: Random string contained from a list of roles.
+    """
+    roles = ["Druid", "Wizard", "Warrior", "Thief", "Warlock", "Death King", "Demon Hunter", "Paladin", "Hunter",
+             "Monk"]
+    sr = random.SystemRandom()
+    return sr.choice(roles)
+
+
+if __name__ == "__main__":
+    print(random_role())
