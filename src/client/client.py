486d81df3ca73c81b5b4a3ca40dce7d8647e917a
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2021-06-21 23:33:11 +0200

    Completed annotations and last big modifies

--- src/client/client.py
+++ src/client/client.py
@@ -1,40 +1,29 @@
-import time
-import tkinter as tk
 from socket import socket, AF_INET, SOCK_STREAM
-from threading import Thread
+from src.utils.app_variables import applicationVariables as appVar
+
+"""
+This class contains basic logics of a client's communication with the server.
+"""
+
 
 class Client:
 
-    buffer_string = ""
-    buffer_size = 4096
-
     def __init__(self, host, port):
         """
-        Estabilish a connection to the main chat game server
+        Establish a connection to the main chat game server.
         :param host: Host of the server
         :param port: Port of the server
         """
         self.socket_instance = socket(AF_INET, SOCK_STREAM)
         self.socket_instance.connect((host, port))
-        # Await a welcome message from the server
-        message = self.socket_instance.recv(Client.buffer_size).decode("utf8")
-        Client.append_string(message)
-        Thread(target=self.__client_loop).start()
 
-    def __client_loop(self):
+    def client_read(self):
         """
         Execute the main loop of the Client.
         This method will try to receive messages from the Server object.
         """
-        new_message = "[New Message]: "
-        error_message = "Something went wrong.."
-        while True:
-            try:
-                message = self.socket_instance.recv(Client.buffer_size).decode("utf8")
-                Client.append_string(new_message + message)
-            except OSError:
-                Client.append_string(error_message)
-                break
+        message = self.socket_instance.recv(appVar.BUFFER_SIZE).decode("utf8")
+        return message
 
     def send_message(self, message):
         """
@@ -43,36 +32,8 @@ class Client:
         """
         self.socket_instance.send(message.encode())
 
-    def append_string(self, message_to_append):
-        Client.buffer_string += message_to_append
-
-    def get_string(self):
-        output = Client.buffer_string
-        Client.buffer_string = ""
-        return output
-
-if __name__ == "__main__":
-
-    my_client = Client("", 53000)
-
-    root = tk.Tk()
-    root.geometry("300x200")
-    root.title("Game Client")
-
-    entry_frame = tk.Frame(root)
-    ent_text = tk.Entry(entry_frame)
-    ent_text.pack(side=tk.LEFT)
-    btn_send = tk.Button(entry_frame, text="Send", command=lambda: my_client.send_message(ent_text.get()))
-    btn_send.pack(side=tk.LEFT)
-    entry_frame.pack(side=tk.BOTTOM)
-
-    chat_Frame = tk.Frame(root)
-    scroll_bar = tk.Scrollbar(chat_Frame)
-    scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-    tk_display = tk.Text(chat_Frame, height=10, width=30)
-    tk_display.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
-    scroll_bar.config(command=tk_display.yview)
-    tk_display.config(yscrollcommand=scroll_bar.set, background="#F4F6F7", highlightbackground="grey", state="disabled")
-    chat_Frame.pack(side=tk.TOP, pady=(5, 10))
-
-    root.mainloop()
+    def close_connection(self):
+        """
+        Close the connection to the main chat game server.
+        """
+        self.socket_instance.close()
---
+++ src/client/gui.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+from threading import Thread
+from src.utils.app_variables import applicationVariables as appVar
+from src.client.client import Client
+
+
+def receive():
+    """
+    This method is used as loop to listen to messages that socket receives.
+    """
+    while True:
+        try:
+            # Receive method uses the client class to listen to socket's messages.
+            message = my_client.client_read()
+            # If message contains the quit command, client's communication must be closed.
+            # Else the message's list is shown on screen.
+            if message == appVar.QUIT_MESSAGE:
+                on_closing()
+            message_list.insert(tk.END, message)
+            # In case an error occurs, probably the client left the chat.
+        except OSError:
+            break
+
+
+def send(event=None):
+    """
+    This method is used to send a message to the server.
+    """
+    # If the user has written a message, it's sent to the server.
+    if len(my_message.get()) > 0:
+        message = my_message.get()
+        # The message is shown on screen.
+        message_list.insert(tk.END, message)
+        # The input box is erased.
+        my_message.set("")
+        # The message is then sent to the server.
+        my_client.send_message(message)
+        # If the message contains the quit command, communications must be interrupted.
+        if message == appVar.QUIT_MESSAGE:
+            my_client.close_connection()
+            root.quit()
+
+
+def on_closing(event=None):
+    """
+    This method is used to close the client's communication with the server.
+    """
+    # Communication is closed by forcing the message to quit command.
+    my_message.set(appVar.QUIT_MESSAGE)
+    send()
+
+
+# *********** GUI PART ***************
+
+root = tk.Tk()
+root.title("Client_GUI")
+
+messages_frame = tk.Frame(root)
+# A StringVar variable to manage messages to sent.
+my_message = tk.StringVar()
+# A scrollbar to show all the messages contained in the ListBox.
+scrollbar = tk.Scrollbar(messages_frame)
+
+# A Listbox to contain all the messages, and a scrollbar attached to see all of them.
+message_list = tk.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+message_list.pack(side=tk.LEFT, fill=tk.BOTH)
+message_list.pack()
+messages_frame.pack()
+
+# The Entry field where user types the messages.
+entry_field = tk.Entry(root, textvariable=my_message)
+# Link the send method to the Return command.
+entry_field.bind("<Return>", send)
+
+entry_field.pack()
+# Link the send method to the send button.
+send_button = tk.Button(root, text="Send", command=send)
+send_button.pack()
+
+# Handel the closing window event.
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+# *********** CONNECTION PART ***************
+
+# Set a new client's communication with the server.
+my_client = Client(appVar.HOST, appVar.PORT)
+# Starting client's loop as the receive method.
+Thread(target=receive).start()
+
+# Start gui.
+tk.mainloop()
